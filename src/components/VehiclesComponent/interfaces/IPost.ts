e062c97f3d272d1a5a047de6cacad1aa2ea8dd21
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-06-19 13:19:49 -0300

    fix 3

--- src/components/VehiclesComponent/interfaces/IPost.ts
+++ src/components/VehiclesComponent/interfaces/IPost.ts
@@ -1,48 +1,49 @@
-import { MapLocationProps } from "@/components/MapLocation/MapLocation";
-import { ITRentalChat } from "@/interfaces/Ichat";
-import { IRentalPost } from "./IRentalPost";
+import { MapLocationProps } from '@/components/MapLocation/MapLocation';
+import { ITRentalChat } from '@/interfaces/Ichat';
+import { IRentalPost } from './IRentalPost';
 
 export interface ICar {
-  id?: string;
-  brand?: string;
-  model?: string;
-  year?: number;
-  mileage?: string;
-  color?: string;
-  availability?: boolean;
-  image_url?: string[];
+	id?: string;
+	brand?: string;
+	model?: string;
+	year?: number;
+	mileage?: string;
+	color?: string;
+	availability?: boolean;
+	image_url?: string[];
 }
 
 export interface IUser {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  phone: string;
-  addresses: {
-    latitude: number | null;
-    longitude: number | null;
-  }[];
+	id: string;
+	name: string;
+	email: string;
+	image_url: string;
+	phone: string;
+	addresses: {
+		latitude: number | null;
+		longitude: number | null;
+	}[];
 }
 
 export interface IReview {
-  id: string;
-  rating: number;
-  comment: string;
-  created_at: string;
-  updated_at: string;
-  user: IUser;
-  post: IPost;
+	id: string;
+	rating: number;
+	comment: string;
+	created_at: string;
+	updated_at: string;
+	user: IUser;
+	post: IPost;
 }
 
 export interface IPost {
-  id: string;
-  title: string;
-  description: string;
-  price: number;
-  user: IUser;
-  car: ICar;
-  review: IReview[];
-  created_at: string;
-  updated_at: string;
+	id: string;
+	title: string;
+	description: string;
+	price: number;
+	user: IUser;
+	car: ICar;
+	review: IReview[];
+	created_at: string;
+	updated_at: string;
+	rentals: IRentalPost[];
 }
