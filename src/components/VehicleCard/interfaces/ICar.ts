18d29c6100ed570bed84760844822c431639a439
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-06-19 14:01:59 -0300

    fix8

--- src/components/VehicleCard/interfaces/ICar.ts
+++ src/components/VehicleCard/interfaces/ICar.ts
@@ -1,12 +1,12 @@
 export interface ICar {
-	id: string;
-	description: string;
+	id?: string;
+	description?: string;
 	carId?: string;
-	carImg: string[];
-	carBrand: string;
-	carModel: string;
-	carYear: number;
-	carMileage: string;
-	carPrice: number;
-	carAvailability: boolean;
+	carImg?: string[];
+	carBrand?: string;
+	carModel?: string;
+	carYear?: number;
+	carMileage?: string;
+	carPrice?: number;
+	carAvailability?: boolean;
 }
